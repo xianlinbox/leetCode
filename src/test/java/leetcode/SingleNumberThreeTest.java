8f4242b7c43a91457ef0bd20ba80027122897ea3
Xianning Liu <xianlinbox@gmail.com>
2016-04-19 23:14:46 +0800

    add SingleNumber iii code: https://leetcode.com/problems/single-number-iii/

---
+++ src/test/java/leetcode/SingleNumberThreeTest.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SingleNumberThreeTest {
+  @Test
+  public void singleNumber() throws Exception {
+    SingleNumberThree solution = new SingleNumberThree();
+
+//    assertEquals(new int[]{1, 3}, solution.singleNumber(new int[]{1, 3}));
+//    assertEquals(new int[]{1, -1}, solution.singleNumber(new int[]{1, 3, 3, -1}));
+//    assertEquals(new int[]{3, 5}, solution.singleNumber(new int[]{1, 2, 1, 3, 2, 5}));
+//    assertEquals(new int[]{1, 5}, solution.singleNumber(new int[]{1, 2, 3, 3, 2, 5}));
+  }
+
+}
